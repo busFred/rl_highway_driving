b0db3a97383725660279a6c0028119e43242f94f
busFred <32878707+busFred@users.noreply.github.com>
2021-10-18 12:42:22 -0400

    add beginer_tutorial

---
+++ src/tutorial/beginner_tutorial.py
@@ -0,0 +1,32 @@
+#%%
+from typing import Dict, Union
+import gym
+from highway_env.envs.highway_env import HighwayEnv
+from highway_env.envs.common.observation import observation_factory
+import matplotlib.pyplot as plt
+
+#%%
+CONFIG: Dict = {
+    "observation": {
+        "type": "Kinematics",
+        "features": ['presence', 'x', 'y', 'vx', 'vy'],
+        "normalize": False,
+        "observe_intentions": False
+    }
+}
+env: HighwayEnv = gym.make("highway-v0")
+env.configure(CONFIG)
+env.reset()
+
+#%%
+for _ in range(100):
+    env.render()
+    action = env.action_space.sample()
+    state, reward, is_terminal, info = env.step(action)
+    if is_terminal is True:
+        print("terminated")
+        env.reset()
+print("simulation end")
+
+#%%
+env.close()
