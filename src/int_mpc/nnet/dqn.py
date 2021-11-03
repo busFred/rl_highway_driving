f3e7a5670c4f11edbb720be310b6d372c6d265be
busFred <32878707+busFred@users.noreply.github.com>
2021-11-03 11:32:12 -0400

    add LinearDQN

--- src/int_mpc/nnet/dqn.py
+++ src/int_mpc/nnet/dqn.py
@@ -1,5 +1,20 @@
 from torch import nn
+import torch
+
+from ..mdps.highway.change_lane import ChangeLaneEnv
 
 
-class DQN(nn.Module):
-    pass
+class LinearDQN(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Flatten(1, -1),
+            nn.Linear(
+                ChangeLaneEnv.STATES_SHAPE[0] * ChangeLaneEnv.STATES_SHAPE[1],
+                100), nn.ReLU(), nn.Linear(100, 100), nn.ReLU(),
+            nn.Linear(100, 100), nn.ReLU(),
+            nn.Linear(100, ChangeLaneEnv.N_ACTIONS))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        output = self.model(x)
+        return output
