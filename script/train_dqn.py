57a417f8ea9fe8cf2ad98019565ab5fc626ab334
busFred <32878707+busFred@users.noreply.github.com>
2021-11-24 19:39:45 -0500

    train dqn metrics_path and checkpoint_path

--- script/train_dqn.py
+++ script/train_dqn.py
@@ -2,7 +2,7 @@ import multiprocessing as mp
 import os
 import sys
 from argparse import ArgumentParser, Namespace
-from typing import Sequence
+from typing import Iterable, Sequence, Union
 
 import pytorch_lightning as pl
 import pytorch_lightning.loggers as pl_loggers
@@ -10,15 +10,17 @@ import torch
 from drl_algs import dqn as alg_dqn
 from int_mpc.mdps.change_lane import ChangeLaneConfig, ChangeLaneEnv
 from int_mpc.nnet.change_lane.dqn import LinearDQN
+from pytorch_lightning.loggers.base import LightningLoggerBase
 
 
 def create_argparse() -> ArgumentParser:
     parser = ArgumentParser()
     parser.add_argument("--env_config_path", type=str, required=True)
     parser.add_argument("--dqn_config_path", type=str, required=True)
-    parser.add_argument("--export_path", type=str, required=True)
     parser.add_argument("--n_val_episodes", type=int, default=20)
     parser.add_argument("--max_workers", type=int, default=None)
+    parser.add_argument("--export_path", type=str, default=None)
+    parser.add_argument("--metrics_path", type=str, default=None)
     parser.add_argument("--checkpoint_path", type=str, default=None)
     return parser
 
@@ -56,8 +58,14 @@ def get_experiment_name(env_config_path: str, dqn_config_path: str) -> str:
 def main(args: Sequence[str]):
     argv: Namespace = parse_args(args)
     # create export path
-    os.makedirs(argv.export_path, exist_ok=True)
-    os.makedirs(argv.checkpoint_path, exist_ok=True)
+    if argv.export_path is not None:
+        os.makedirs(argv.export_path, exist_ok=True)
+    if argv.metrics_path is not None:
+        os.makedirs(argv.metrics_path, exist_ok=True)
+    if argv.checkpoint_path is not None:
+        os.makedirs(argv.checkpoint_path, exist_ok=True)
+        os.makedirs(os.path.join(argv.metrics_path, "csv"), exist_ok=True)
+        os.makedirs(os.path.join(argv.metrics_path, "tfb"), exist_ok=True)
     # configure environment
     env_config: ChangeLaneConfig = get_env_config(argv.env_config_path)
     env = ChangeLaneEnv(env_config)
@@ -74,12 +82,18 @@ def main(args: Sequence[str]):
     # configure experiment name
     exp_name: str = get_experiment_name(argv.env_config_path,
                                         argv.dqn_config_path)
-    loggers = [
-        pl_loggers.TensorBoardLogger(save_dir=argv.export_path,
-                                     name=exp_name + "_tfb",
-                                     default_hp_metric=False),
-        pl_loggers.CSVLogger(save_dir=argv.export_path, name=exp_name + "_csv")
-    ]
+    loggers: Union[LightningLoggerBase, Iterable[LightningLoggerBase],
+                   bool] = False
+    if argv.metrics_path is not None:
+        loggers = [
+            pl_loggers.CSVLogger(save_dir=os.path.join(argv.metrics_path,
+                                                       "csv"),
+                                 name=exp_name),
+            pl_loggers.TensorBoardLogger(save_dir=os.path.join(
+                argv.metrics_path, "tfb"),
+                                         name=exp_name,
+                                         default_hp_metric=False)
+        ]
     # train the dqn
     trainer = pl.Trainer(max_epochs=dqn_config.n_episodes,
                          logger=loggers,
@@ -90,8 +104,9 @@ def main(args: Sequence[str]):
                          default_root_dir=argv.checkpoint_path)
     trainer.fit(dqn)
     # export model
-    model_path: str = os.path.join(argv.export_path, exp_name + ".pt")
-    torch.save(dqn.dqn, model_path)
+    if argv.export_path is not None:
+        model_path: str = os.path.join(argv.export_path, exp_name + ".pt")
+        torch.save(dqn.dqn, model_path)
 
 
 if __name__ == "__main__":
