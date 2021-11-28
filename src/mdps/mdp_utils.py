c19779edaf6647f579edce40c620ff228b6e9751
busFred <32878707+busFred@users.noreply.github.com>
2021-11-28 16:54:10 -0500

    simulate_episodes add timeout

--- src/mdps/mdp_utils.py
+++ src/mdps/mdp_utils.py
@@ -51,22 +51,30 @@ def simulate_episodes(env: Environment,
                       max_episode_steps: int,
                       n_episodes: int,
                       to_visualize: bool = True,
-                      max_workers: Optional[int] = None) -> Sequence[Metrics]:
+                      max_workers: Optional[int] = None,
+                      timeout: float = 15 * 60) -> Sequence[Metrics]:
     """Simulate the MDP with the given policy.
 
     Args:
         env (Environment): The enviornment to be simulated.
         policy (PolicyBase): The policy used to generate actions.
         max_episode_steps (int): The maximum number of episodes.
-        n_episodes (int): [description]
+        n_episodes (int): Number of episodes.
         to_visualize (bool, optional): Whether to visualize the enviorinment. Defaults to True.
+        max_workers (int): Maximum process to use.
+        timeout (float): Amount of time to kill the child processes if it returns nothing.
 
     Returns:
         metrics (Sequence[Metrics]): The metrics of the all the episodes.
     """
-    with mp.Pool(processes=max_workers) as pool:
-        metrics: Sequence[Metrics] = pool.starmap(
+    with mp.get_context("spawn").Pool(processes=max_workers) as pool:
+        metrics: Sequence[Metrics] = list()
+        result = pool.starmap_async(
             simulate, [(env.new_env_like(), copy.deepcopy(policy),
                         max_episode_steps, to_visualize)
                        for _ in range(n_episodes)])
+        try:
+            metrics = result.get(timeout=timeout)
+        except TimeoutError as te:
+            raise te
     return metrics
