73a0589e095a5330e12911ff81a1766dd7c7455f
busFred <32878707+busFred@users.noreply.github.com>
2022-02-11 14:37:22 -0500

    use TrainDQNConfigs

--- script/train_dqn.py
+++ script/train_dqn.py
@@ -2,112 +2,136 @@ import multiprocessing as mp
 import os
 import sys
 from argparse import ArgumentParser, Namespace
-from typing import Iterable, Sequence, Union
+from typing import Sequence, Tuple, Union
+from attr import field
 
 import pytorch_lightning as pl
 import pytorch_lightning.loggers as pl_loggers
 import torch
 from drl_algs import dqn as alg_dqn
 from int_mpc.mdps.change_lane import ChangeLaneConfig, ChangeLaneEnv
-from int_mpc.nnet.change_lane.dqn import LinearDQN
-from pytorch_lightning.loggers.base import LightningLoggerBase
+from int_mpc.nnet.change_lane.dqn import LinearDQN, LinearDQNConfig
+import script_common
 
 
-def create_argparse() -> ArgumentParser:
-    parser = ArgumentParser()
-    parser.add_argument("--env_config_path", type=str, required=True)
-    parser.add_argument("--dqn_config_path", type=str, required=True)
-    parser.add_argument("--n_val_episodes", type=int, default=20)
-    parser.add_argument("--lr", type=float, default=1e-3)
-    parser.add_argument("--max_workers", type=int, default=None)
-    parser.add_argument("--export_path", type=str, default=None)
-    parser.add_argument("--metrics_path", type=str, default=None)
-    parser.add_argument("--checkpoint_path", type=str, default=None)
-    return parser
-
-
-def parse_args(args: Sequence[str]):
-    parser = create_argparse()
-    argv = parser.parse_args(args)
-    return argv
-
-
-def get_env_config(env_config_path: str):
+class TrainDQNConfigs:
+    # configs from json files
     env_config: ChangeLaneConfig
-    with open(env_config_path, "r") as config_file:
-        env_config = ChangeLaneConfig.from_json(config_file.read())
-    return env_config
-
-
-def get_dqn_config(dqn_config_path: str):
     dqn_config: alg_dqn.DQNConfig
-    with open(dqn_config_path, "r") as config_file:
-        dqn_config = alg_dqn.DQNConfig.from_json(config_file.read())
-    return dqn_config
+    dqn_net_config: LinearDQNConfig
+    # training configs
+    n_val_episodes: int = field(default=20)
+    lr: float = field(default=1e-3)
+    # experiment name, version, and export path
+    experiment_name: str
+    experiments_root_path: str
+    experiment_path: str  # experiments_root_path/experiment_name/version_{version}
+    checkpoint_path: str  # experiment_path/checkpoints
+    model_export_path_fn: str  # experiment_path/{experiment_name}.pt
+    version: int
+    # accelerate computing
+    max_workers: Union[int, None]
+    use_gpu: bool
+    # loggers
+    loggers: Tuple[pl_loggers.CSVLogger, pl_loggers.TensorBoardLogger]
 
+    # experiments_root_path/experiment_name/version_{:02d}
 
-def get_experiment_name(env_config_path: str, dqn_config_path: str) -> str:
-    env_conf_filename: str = os.path.split(env_config_path)[-1]
-    env_conf_filename = os.path.splitext(env_conf_filename)[0]
-    dqn_conf_filename: str = os.path.split(dqn_config_path)[-1]
-    dqn_conf_filename = os.path.splitext(dqn_conf_filename)[0]
-    exp_name: str = str.format("env_{}_dqn_{}", env_conf_filename,
-                               dqn_conf_filename)
-    return exp_name
+    def __init__(self, args: Sequence[str]) -> None:
+        argv = self._parse_args(args)
+        self._load_configs(argv)
+        self.n_val_episodes = argv.n_val_episodes
+        self.lr = argv.lr
+        self.experiment_name = argv.experiment_name
+        self.experiments_root_path = argv.experiments_root_path
+        self._configure_loggers()
+        self._configure_export_paths()
+        self.max_workers = argv.max_workers
+        self.use_gpu = argv.use_gpu
+
+    def _parse_args(self, args: Sequence[str]):
+        parser = ArgumentParser()
+        parser.add_argument("--env_config_path", type=str, required=True)
+        parser.add_argument("--dqn_config_path", type=str, required=True)
+        parser.add_argument("--dqn_net_config_path", type=str, required=True)
+        parser.add_argument("--n_val_episodes", type=int, default=20)
+        parser.add_argument("--lr", type=float, default=1e-3)
+        parser.add_argument("--experiment_name", type=str, default="untitiled")
+        parser.add_argument("--experiments_root_path",
+                            type=str,
+                            default="experiments")
+        parser.add_argument("--max_workers", type=int, default=None)
+        parser.add_argument("--use_gpu", action="store_true")
+        argv = parser.parse_args(args)
+        return argv
+
+    def _load_configs(self, argv: Namespace):
+        self.env_config = script_common.get_env_config(argv.env_config_path)
+        self.dqn_config = self._get_dqn_config(argv.dqn_config_path)
+        self.dqn_net_config = self._get_dqn_net_config(
+            argv.dqn_net_config_path)
+
+    def _get_dqn_config(self, dqn_config_path: str):
+        dqn_config: alg_dqn.DQNConfig
+        with open(dqn_config_path, "r") as config_file:
+            dqn_config = alg_dqn.DQNConfig.from_json(config_file.read())
+        return dqn_config
+
+    def _get_dqn_net_config(self, dqn_net_config_path: str):
+        net_config: LinearDQNConfig
+        with open(dqn_net_config_path, "r") as config_file:
+            net_config = LinearDQNConfig.from_json(config_file.read())
+        return net_config
+
+    def _configure_loggers(self):
+        tfb_logger = pl_loggers.TensorBoardLogger(
+            save_dir=self.experiments_root_path,
+            name=self.experiment_name,
+            sub_dir="tfb",
+            default_hp_metric=False)
+        csv_logger = pl_loggers.CSVLogger(save_dir=self.experiments_root_path,
+                                          name=self.experiment_name,
+                                          version=tfb_logger.version)
+        self.loggers = (csv_logger, tfb_logger)
+        self.version = tfb_logger.version
+
+    def _configure_export_paths(self):
+        self.experiment_path = self.loggers[0].log_dir
+        self.checkpoint_path = os.path.join(self.experiment_path,
+                                            "checkpoints")
+        self.model_export_path_fn = os.path.join(self.experiment_path,
+                                                 self.experiment_name + ".pt")
+        os.makedirs(self.experiment_path, exist_ok=True)
 
 
 def main(args: Sequence[str]):
-    argv: Namespace = parse_args(args)
-    # create export path
-    if argv.export_path is not None:
-        os.makedirs(argv.export_path, exist_ok=True)
-    if argv.metrics_path is not None:
-        os.makedirs(argv.metrics_path, exist_ok=True)
-    if argv.checkpoint_path is not None:
-        os.makedirs(argv.checkpoint_path, exist_ok=True)
-        os.makedirs(os.path.join(argv.metrics_path, "csv"), exist_ok=True)
-        os.makedirs(os.path.join(argv.metrics_path, "tfb"), exist_ok=True)
-    # configure environment
-    env_config: ChangeLaneConfig = get_env_config(argv.env_config_path)
-    env = ChangeLaneEnv(env_config)
-    # get dqn
-    dqn_config: alg_dqn.DQNConfig = get_dqn_config(argv.dqn_config_path)
-    net = LinearDQN()
-    net.reset_parameters()
-    dqn = alg_dqn.DQNTrain(env=env,
-                           dqn_net=net,
-                           dqn_config=dqn_config,
-                           max_episode_steps=env_config.max_episode_steps,
-                           n_val_episodes=argv.n_val_episodes,
-                           optimizer=torch.optim.Adam(net.parameters()),
-                           max_workers=argv.max_workers)
-    # configure experiment name
-    exp_name: str = get_experiment_name(argv.env_config_path,
-                                        argv.dqn_config_path)
-    loggers: Union[LightningLoggerBase, Iterable[LightningLoggerBase],
-                   bool] = False
-    if argv.metrics_path is not None:
-        loggers = [
-            pl_loggers.CSVLogger(save_dir=os.path.join(argv.metrics_path,
-                                                       "csv"),
-                                 name=exp_name),
-            pl_loggers.TensorBoardLogger(save_dir=os.path.join(
-                argv.metrics_path, "tfb"),
-                                         name=exp_name,
-                                         default_hp_metric=False)
-        ]
-    # train the dqn
-    trainer = pl.Trainer(max_epochs=dqn_config.n_episodes,
-                         logger=loggers,
-                         gpus=-1,
-                         auto_select_gpus=True,
+    # load configurations from command line arguments
+    configs = TrainDQNConfigs(args)
+    # create environment
+    env = ChangeLaneEnv(configs.env_config)
+    # create dqn_net
+    dqn_net = LinearDQN(configs.dqn_net_config)
+    dqn_net.init(env)
+    #  configure optimizer
+    optim = torch.optim.Adam(dqn_net.parameters(), lr=configs.lr)
+    # create dqn algorithm with dqn_net
+    dqn = alg_dqn.DQNTrain(
+        env=env,
+        dqn_net=dqn_net,
+        dqn_config=configs.dqn_config,
+        max_episode_steps=configs.env_config.max_episode_steps,
+        n_val_episodes=configs.n_val_episodes,
+        optimizer=optim,
+        max_workers=configs.max_workers)
+    # train dqn
+    trainer = pl.Trainer(max_epochs=configs.dqn_config.n_episodes,
+                         logger=configs.loggers,
+                         auto_select_gpus=configs.use_gpu,
                          check_val_every_n_epoch=5,
-                         default_root_dir=argv.checkpoint_path)
+                         default_root_dir=configs.checkpoint_path)
     trainer.fit(dqn)
-    # export model
-    if argv.export_path is not None:
-        model_path: str = os.path.join(argv.export_path, exp_name + ".pt")
-        torch.save(dqn.dqn, model_path)
+    # save final dqn
+    torch.save(dqn.dqn, configs.model_export_path_fn)
 
 
 if __name__ == "__main__":
