7091c8a4269162db680db4458feb4939fe8735f6
busFred <32878707+busFred@users.noreply.github.com>
2022-02-11 14:37:03 -0500

    LinearDQN flexible module architecture

--- src/int_mpc/nnet/change_lane/dqn.py
+++ src/int_mpc/nnet/change_lane/dqn.py
@@ -1,31 +1,56 @@
-from typing import Any, Callable, Optional
-from torch import nn
+from dataclasses import dataclass, field
+from typing import List, Literal
+
 import torch
+from dataclasses_json import dataclass_json
+from mdps import mdp_abc, mdp_utils
+from torch import nn
 
 from ...mdps.change_lane import ChangeLaneEnv
 
 
-class LinearDQN(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.model = nn.Sequential(
-            nn.Flatten(1, -1),
-            nn.Linear(
-                ChangeLaneEnv.STATES_SHAPE[0] * ChangeLaneEnv.STATES_SHAPE[1],
-                100), nn.ReLU(), nn.Linear(100, 100), nn.ReLU(),
-            nn.Linear(100, 100), nn.ReLU(),
-            nn.Linear(100, ChangeLaneEnv.N_ACTIONS))
+@dataclass_json
+@dataclass
+class LinearDQNConfig:
+    out_features: List[int] = field()
+    use_biases: List[bool] = field()
+    use_batch_norms: List[bool] = field()
+    use_activations: List[bool] = field()
+    activation_type: Literal["relu", "tanh", "none"] = field()
 
-    def reset_parameters(self,
-                         initializer: Optional[Callable[[torch.Tensor],
-                                                        Any]] = None):
-        for m in self.model.modules():
-            if isinstance(m, nn.Linear):
-                if initializer is None:
-                    m.reset_parameters()
-                else:
-                    initializer(m.weight)
+
+class LinearDQN(nn.Module):
+    model: nn.ModuleList
+
+    def __init__(self, configs: LinearDQNConfig) -> None:
+        super().__init__()
+        model = nn.ModuleList([nn.Flatten()])
+        for out_features, use_bias, use_batch_norm, use_activation in zip(
+                configs.out_features, configs.use_biases,
+                configs.use_batch_norms, configs.use_activations):
+            # add linear layer
+            model.append(nn.LazyLinear(out_features, use_bias))
+            # add batch norm
+            if use_batch_norm:
+                model.append(nn.LazyBatchNorm1d())
+            # add activation
+            if configs.activation_type != "none" and use_activation:
+                if configs.activation_type == "relu":
+                    model.append(nn.ReLU())
+                elif configs.activation_type == "tanh":
+                    model.append(nn.Tanh())
+        model.append(nn.LazyLinear(ChangeLaneEnv.N_ACTIONS))
+        self.model = model
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        output = self.model(x)
+        output = x
+        for layer in self.model:
+            output = layer(output)
         return output
+
+    def init(self, env: mdp_abc.Environment):
+        state = env.reset()
+        dummy_input = mdp_utils.states_to_torch([state],
+                                                dtype=torch.float,
+                                                device=torch.device("cpu"))
+        self.forward(dummy_input)
