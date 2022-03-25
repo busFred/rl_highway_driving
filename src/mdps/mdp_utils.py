8246101452b6c0696274c0bb9c16ff95d73f21ed
busFred <32878707+busFred@users.noreply.github.com>
2022-03-25 18:29:23 -0400

    update simulate_episodes to use apply_async

--- src/mdps/mdp_utils.py
+++ src/mdps/mdp_utils.py
@@ -1,8 +1,9 @@
-from typing import Optional, Sequence
+from multiprocessing.pool import AsyncResult
+from typing import List, Optional, Sequence
 
 import numpy as np
 import torch
-from torch import multiprocessing as mp
+import torch.multiprocessing as mp
 
 from .mdp_abc import Action, Environment, Metrics, PolicyBase, State
 
@@ -51,7 +52,7 @@ def simulate_episodes(env: Environment,
                       n_episodes: int,
                       to_visualize: bool = True,
                       max_workers: Optional[int] = None,
-                      timeout: float = 15 * 60) -> Sequence[Metrics]:
+                      timeout: float = 15) -> Sequence[Metrics]:
     """Simulate the MDP with the given policy.
 
     Args:
@@ -61,22 +62,42 @@ def simulate_episodes(env: Environment,
         n_episodes (int): Number of episodes.
         to_visualize (bool, optional): Whether to visualize the enviorinment. Defaults to True.
         max_workers (int): Maximum process to use.
-        timeout (float): Amount of time to kill the child processes if it returns nothing.
+        timeout (float): Amount of time in minute to kill the child processes if it returns nothing.
 
     Returns:
         metrics (Sequence[Metrics]): The metrics of the all the episodes.
     """
+    results = list()
+    if max_workers == 1:
+        for _ in range(n_episodes):
+            metric = simulate(env, policy, max_episode_steps, to_visualize)
+            results.append(metric)
     with mp.get_context("spawn").Pool(processes=max_workers) as pool:
-        result = pool.starmap_async(
-            simulate,
-            [(env.new_env_like(), policy, max_episode_steps, to_visualize)
-             for _ in range(n_episodes)])
+        mp_res: List[AsyncResult] = list()
+        for _ in range(n_episodes):
+            r = pool.apply_async(
+                simulate,
+                (env.new_env_like(), policy, max_episode_steps, to_visualize))
+            mp_res.append(r)
+        pool.close()
+        for r in mp_res:
+            r.wait(timeout * 60)
+        pool.terminate()
+        for r in mp_res:
+            try:
+                metric = r.get()
+                results.append(metric)
+            except TimeoutError:
+                pass
+        # pool.terminate()
+        # mp_res = pool.starmap_async(
+        #     simulate,
+        #     [(env.new_env_like(), policy, max_episode_steps, to_visualize)
+        #      for _ in range(n_episodes)],
+        #     callback=partial(_metric_collector, result=results))
         # result = pool.starmap_async(
         #     simulate, [(env.new_env_like(), copy.deepcopy(policy),
         #                 max_episode_steps, to_visualize)
         #                for _ in range(n_episodes)])
-        result.wait(timeout=timeout)
-        if result.ready():
-            return result.get()
-        else:
-            return list()
+        # mp_res.wait(timeout=timeout * 60 * n_episodes)
+    return results
